06491139233eb569914be921619288395cd80e42
Ayub Khan <ayubk0353@gmail.com>
2022-05-18 11:47:25 +0530

    rectification

--- app/src/main/java/com/khntech/dietchart/MainActivity.kt
+++ app/src/main/java/com/khntech/dietchart/MainActivity.kt
@@ -7,15 +7,21 @@ import com.khntech.dietchart.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityMainBinding
+    ///private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+      //  binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
 
 
     }
-
 }
+
+
+
+
+
+       
+
