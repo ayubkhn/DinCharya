6fb7c999bd48b527b53e6077d96d7219940cdcec
Ayub Khan <ayubk0353@gmail.com>
2022-05-18 11:52:08 +0530

    code has been modified...

--- app/src/main/java/com/khntech/dietchart/MainActivity.kt
+++ app/src/main/java/com/khntech/dietchart/MainActivity.kt
@@ -2,17 +2,11 @@ package com.khntech.dietchart
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.khntech.dietchart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
-    ///private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-      //  binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
 
